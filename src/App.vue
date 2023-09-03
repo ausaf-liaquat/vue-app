c1adddb05b4ca1cf7ea796eed69b57d4dad34d3b
Ausaf Liaquat <ausaf.wpt@gmail.com>
2023-09-03 22:01:57 +0500

    vue cli section completed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <h1>Vue Component</h1>
-  <div v-if="show">
+  <teleport to=".modals" v-if="show">
     <Modal theme="sale" @close="toggleModal">
-      <h1>Ausaf Vue Project</h1>
+      <h1>Ausaf Vue Project!</h1>
       <p>Grab your Chance</p>
       <template v-slot:links>
         <a href="#">Sign In </a>
         <a href="#">Sign Up </a>
       </template>
     </Modal>
+  </teleport>
+  <div v-if="showTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Ausaf Vue Project 2!</h1>
+      <p>Grab your Chance 2</p>
+      <!-- <template v-slot:links>
+        <a href="#">Sign In </a>
+        <a href="#">Sign Up </a>
+      </template> -->
+    </Modal>
   </div>
   <button @click="toggleModal">Show Modal</button>
+
+  <button  @click="toggleModalTwo">Show Modal 2</button>
 </template>
 
 <script>
@@ -21,10 +33,8 @@ export default {
   name: "App",
   data() {
     return {
-      title: "My First Component",
-      header: "First Component",
-      text: "Grab Your Chance",
       show: false,
+      showTwo: false,
     };
   },
   components: {
@@ -33,6 +43,9 @@ export default {
   methods: {
     toggleModal() {
       this.show = !this.show;
+    },
+    toggleModalTwo() {
+      this.showTwo = !this.showTwo;
     },
   },
 };
@@ -47,9 +60,18 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.modals {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 button {
   padding: 10px;
   border: 0px;
   border-radius: 6px;
+  margin: 4px;
 }
 </style>
